738422c8ee4daecdc8384b20692745733c42017f
shines77 <gz_shines@msn.com>
2019-02-21 01:40:02 +0800

    run.bat: 注释一句 cd 语句.
    Signed-off-by: shines77 <gz_shines@msn.com>

--- java/Fibonacci/run.bat
+++ java/Fibonacci/run.bat
@@ -5,7 +5,7 @@ javac .\src\net\i77soft\algorithm\Program.java
 cd .\src\
 java -Xint net.i77soft.algorithm.Program
 
-cd ..\release\
+@rem cd ..\release\
 @rem java -Xint -classpath .;fibonacci.jar net.i77soft.algorithm.Program
 
 pause
