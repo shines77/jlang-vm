9a5ab229f7ef5212d527a4771783521f94e03b16
shines77 <gz_shines@msn.com>
2019-05-09 07:46:57 +0800

    改进 custom_onetime_ptr<T> 类;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/main/jlang/asm/ScriptNode.h
+++ src/main/jlang/asm/ScriptNode.h
@@ -9,7 +9,16 @@
 #include "jlang/basic/stddef.h"
 #include "jlang/lang/NonCopyable.h"
 
+#include <stdint.h>
+#include <stddef.h>
+#include <malloc.h>
+#include <memory.h>
+
+#include <cstddef>
+#include <cstdint>
 #include <type_traits>
+#include <memory>
+#include <xmemory>
 #include <functional>
 
 namespace jlang {
@@ -145,24 +154,149 @@ public:
     }
 };
 
+inline
+void * _Allocate(std::size_t element_count, std::size_t element_size,
+		         bool try_aligned_allocation = true,
+                 std::size_t alignment = 8) {
+    void * ptr = nullptr;
+
+    if (element_count == 0)
+        return ptr;
+
+    const std::size_t total_size = element_size * element_count;
+    if (try_aligned_allocation) {
+#ifdef _WIN32
+        ptr = _aligned_malloc(total_size, alignment);
+#else
+        ptr = memalign(alignment, total_size);
+#endif
+    }
+    else {
+        ptr = ::operator new(total_size);
+    }
+    return ptr;
+}
+
 template <typename T>
+class allocator {
+public:
+    typedef typename std::remove_const<
+                typename std::remove_pointer<T>::type
+            >::type value_type;
+
+    typedef value_type *        pointer;
+    typedef const value_type *  const_pointer;
+
+    typedef value_type &        reference;
+    typedef const value_type &  const_reference;
+
+    typedef std::size_t         size_type;
+    typedef std::ptrdiff_t      difference_type;
+
+    typedef true_type           propagate_on_container_move_assignment;
+    typedef true_type           is_always_equal;
+
+    typedef allocator<T>        this_type;
+
+public:
+    allocator() {}
+    allocator(const this_type &) {}
+
+    template <typename Other>
+    allocator(const allocator<Other> &) {}
+
+    template <typename Other>
+    this_type & operator = (const allocator<Other> &) {
+        return *this;
+    }
+
+    ~allocator() {}
+
+    pointer address(reference value) const {
+        return std::addressof(value);
+    }
+
+    const_pointer address(const_reference value) const {
+        return std::addressof(value);
+    }
+
+    pointer allocate(size_type count) {
+        // TODO:
+        return static_cast<pointer>(_Allocate(count, sizeof(value_type)));
+    }
+
+    pointer allocate(size_type count, const void *) {
+        return this->allocate(count);
+    }
+
+    void deallocate(pointer ptr, size_type count) {
+        // TODO:
+        //_Deallocate(ptr, count, sizeof(value_type));
+    }
+
+    inline pointer create() {
+        pointer value = new value_type();
+        return value;
+    }
+
+    template <typename ...Args>
+    inline pointer create(Args && ... args) {
+        pointer value = new value_type(std::forward<Args>(args)...);
+        return value;
+    }
+
+    inline void destroy(pointer ptr, size_type size) {
+        assert(ptr != nullptr);
+        assert(size = sizeof(value_type));
+        delete ptr;
+    }
+    
+    inline void destroy(pointer ptr, size_type size, const void *) {
+        assert(ptr != nullptr);
+        assert(size = sizeof(value_type));
+        delete[] ptr;
+    }
+
+    inline void constructor(pointer ptr) {
+        assert(ptr != nullptr);
+        void * vptr = static_cast<void *>(ptr);
+        ::new (vptr) value_type();
+    }
+
+    template <typename ...Args>
+    inline void constructor(pointer ptr, Args && ... args) {
+        assert(ptr != nullptr);
+        void * vptr = static_cast<void *>(ptr);
+        ::new (vptr) value_type(std::forward<Args>(args)...);
+    }
+
+    template <typename U>
+    inline void destructor(U * ptr) {
+        assert(ptr != nullptr);
+        ptr->~U();
+    }
+
+    size_type max_size() const {
+        // Estimate maximum array size
+        return ((size_type)(-1) / sizeof(value_type));
+    }
+};
+
+template <typename T, typename Allocator = allocator<T>>
 class custom_onetime_ptr : public pure_type<T>::type {
 public:
-    typedef typename pure_type<T>::type element_type;
+    typedef typename pure_type<T>::type     element_type;
+    typedef Allocator                       allocator_type;
 
     typedef std::function<void(element_type *, size_t)> deleter_type;
 
 protected:
     element_type * value_;
     bool shifted_;
-    deleter_type deleter_;
+    allocator_type allocator_;
 
 public:
     custom_onetime_ptr(element_type * value = nullptr) : value_(value), shifted_(false) {}
-    custom_onetime_ptr(const deleter_type & deleter) : value_(nullptr),
-        shifted_(false), deleter_(deleter) {}
-    custom_onetime_ptr(element_type * value, const deleter_type & deleter)
-        : value_(value), shifted_(false), deleter_(deleter) {}
     custom_onetime_ptr(custom_onetime_ptr & src) {
         this->assign(src);
     }
@@ -187,7 +321,7 @@ private:
     void internal_destroy() {
         if (this->value_) {
             assert(this->deleter_);
-            this->deleter_(this->value_, sizeof(element_type));
+            this->allocator_.destroy(this->value_, sizeof(element_type));
             this->value_ = nullptr;
         }
     }
@@ -222,14 +356,12 @@ private:
 
 public:
     element_type * value() const { return this->value_; }
-    element_type * deleter() const { return this->deleter_; }
 
     template <typename ...Args>
     JM_FORCEINLINE void create_new(Args && ... args) {
         if (!this->shifted_) {
             if (this->value_) {
-                assert(this->deleter_);
-                this->deleter_(this->value_, sizeof(element_type));
+                this->allocator_.destroy(this->value_, sizeof(element_type));
             }
         }
         this->value_ = new element_type(std::forward<Args>(args)...);
@@ -273,10 +405,6 @@ public:
             this->destroy();
             this->shifted_ = false;
         }
-    }
-
-    void set_deleter(const deleter_type & deleter) {
-        this->deleter_ = deleter;
     }
 
     bool is_shifted() const {
