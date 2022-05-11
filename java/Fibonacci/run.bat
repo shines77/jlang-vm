7835bf1d5eb5f918c6059d5743ae6b9813f36fdb
XiongHui Guo <gz_shines@msn.com>
2022-05-11 11:52:09 +0800

    为 /java/Fibonacci/ 的 run 脚本添加 "Disabling the JIT" 的注释
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

--- java/Fibonacci/run.bat
+++ java/Fibonacci/run.bat
@@ -2,10 +2,16 @@
 
 javac .\src\net\i77soft\algorithm\Program.java
 
+::
+:: Disabling the JIT
+:: https://www.ibm.com/docs/en/sdk-java-technology/8?topic=compiler-disabling-jit
+::
+
 cd .\src\
+:: java -Djava.compiler=NONE net.i77soft.algorithm.Program
 java -Xint net.i77soft.algorithm.Program
 
-@rem cd ..\release\
-@rem java -Xint -classpath .;fibonacci.jar net.i77soft.algorithm.Program
+:: cd ..\release\
+:: java -Xint -classpath .;fibonacci.jar net.i77soft.algorithm.Program
 
 pause
--- java/Fibonacci/run.sh
+++ java/Fibonacci/run.sh
@@ -2,6 +2,12 @@
 
 javac ./src/net/i77soft/algorithm/Program.java
 
+##
+## Disabling the JIT
+## https://www.ibm.com/docs/en/sdk-java-technology/8?topic=compiler-disabling-jit
+##
+
 cd ./src/
 # java net.i77soft.algorithm.Program
+# java -Djava.compiler=NONE net.i77soft.algorithm.Program
 java -Xint net.i77soft.algorithm.Program
