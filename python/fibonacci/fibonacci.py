1d319af40d0d2db71ae177857a9dabc2a34196be
XiongHui Guo <gz_shines@msn.com>
2022-05-11 13:08:11 +0800

    改进 python 的 fibonacci.py 脚本, 以自动适应 python 2.x 和 python 3.x
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

--- python/fibonacci/fibonacci.py
+++ python/fibonacci/fibonacci.py
@@ -4,28 +4,77 @@
 #coding=utf-8
 
 import datetime
+import time
 import re
+import os, sys
 
 def fibonacci(n):
-    if (n >= 3):
-        return fibonacci(n - 1) + fibonacci(n - 2);
+    if n >= 3:
+        return fibonacci(n - 1) + fibonacci(n - 2)
     else:
-        return 1;
+        return 1
 
-print('');
+def python2_fibonacci_test():
+    print('')
 
-# 'scanf() %u'
-n = input('Input a number (1-50): ? ');
-print('');
+    # 'scanf() %u'
+    num = raw_input('Input a number (n = 1-40): ? ');
+    print('')
 
-pattern = re.compile(r"\d+");
-match = pattern.match(str(n));
-if match:
-    # print(match.group());
+    pattern = re.compile(r"\d+")
+    match = pattern.match(str(num))
+    if match:
+        # print(match.group())
+        n = int(num)
 
-    print(datetime.datetime.now());
-    fib_n = fibonacci(n);
-    print(datetime.datetime.now());
-    print('');
+        if n > 40:
+            print('Error: The n value is bigger than 40.')
+            os._exit(1)
 
-    print('fibonacci(%d) = %d' % (n, fib_n));
+        print(datetime.datetime.now())
+        time_start = time.time()
+        fib_n = fibonacci(n)
+        time_end = time.time()
+        print(datetime.datetime.now())
+        print('')
+
+        print('fibonacci(%d) = %d' % (n, fib_n))
+        print('')
+        print('elapsed time: %0.2f ms' % ((time_end - time_start) * 1000))
+    else:
+        print('Error: Please input a integer number.')
+
+def python3_fibonacci_test():
+    print('')
+
+    # 'scanf() %u'
+    num = input('Input a number (n = 1-40): ? ');
+    print('')
+
+    pattern = re.compile(r"\d+")
+    match = pattern.match(str(num))
+    if match:
+        # print(match.group())
+        n = int(num)
+
+        if n > 40:
+            print('Error: The n value is bigger than 40.')
+            os._exit(1)
+
+        print(datetime.datetime.now())
+        time_start = time.time()
+        fib_n = fibonacci(n)
+        time_end = time.time()
+        print(datetime.datetime.now())
+        print('')
+
+        print('fibonacci(%d) = %d' % (n, fib_n))
+        print('')
+        print('elapsed time: %0.2f ms' % ((time_end - time_start) * 1000))
+    else:
+        print('Error: Please input a integer number.')
+
+if sys.version_info[0] >= 3:
+    python3_fibonacci_test()
+else:
+    python2_fibonacci_test()
---
+++ python/run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+python .\fibonacci\fibonacci.py
+
+pause
